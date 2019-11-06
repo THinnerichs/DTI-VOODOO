d048ee847c0ec0999f8549d07cae332f92fe1591
THinnerichs <tilman@hinnerichs.com>
2019-11-06 10:50:31 +0100

    Writing protein seq for paracetamol to file 2

--- src/dti_predictor.py
+++ src/dti_predictor.py
@@ -7,10 +7,13 @@ def main():
 
 
     # Create protein amino acid sequence from fasta
+    print("Reading fasta file to dict...")
     fasta_filename = "../data/protein.sequences.v10.fa"
     protein_aa_seq_dict = SeqIO.index(fasta_filename, 'fasta')
+    print("Finished.")
 
     # process drug-protein-interaction file
+    print("Processing drug protein data...")
     protein_chemical_links_filename = "../data/protein_chemical.links.transfer.v5.0.tsv"
     paracetamol_id = 'CID1983'
     counter = 0
@@ -36,7 +39,8 @@ def main():
             seqio_seq = protein_aa_seq_dict[organism+'.'+protein]
 
             with open(file="../data/para_targets", mode='a') as para_handler:
-                para_handler.write(organism+'.'+protein + "\t" + seqio_seq.seq)
+                para_handler.write(organism+'.'+protein + "\t" + seqio_seq.seq+"\n")
+    print("Finished.")
 
 
 
