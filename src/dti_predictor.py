c63e32fed249bb3df5f73694d9e7dfef98f778b3
THinnerichs <tilman@hinnerichs.com>
2019-11-06 10:48:14 +0100

    Writing protein seq for paracetamol to file

--- src/dti_predictor.py
+++ src/dti_predictor.py
@@ -1,27 +1,48 @@
 import numpy as np
 
+from Bio import SeqIO
+
 
 def main():
-    protein_chemical_links_filename = "../data/protein_chemical.links.transfer.v5.0.tsv"
 
-    paracetamol_id = "CID1983"
+
+    # Create protein amino acid sequence from fasta
+    fasta_filename = "../data/protein.sequences.v10.fa"
+    protein_aa_seq_dict = SeqIO.index(fasta_filename, 'fasta')
+
+    # process drug-protein-interaction file
+    protein_chemical_links_filename = "../data/protein_chemical.links.transfer.v5.0.tsv"
+    paracetamol_id = 'CID1983'
     counter = 0
+    paracetamol_prots_count = 0
     with open(file=protein_chemical_links_filename, mode='r') as f:
         for line in f:
             counter += 1
+            paracetamol_prots_count += 1
+
             if counter%100000==0:
                 print("Processed lines: {}\r".format(counter))
 
-            '''
             if paracetamol_id not in line:
                 continue
+
             split_line = line.strip().split('\t')
+            if int(split_line[10]) < 700:
+                continue
+
             drug = split_line[0]
             organism, protein = split_line[1].strip().split('.')
-            '''
+
+            seqio_seq = protein_aa_seq_dict[organism+'.'+protein]
+
+            with open(file="../data/para_targets", mode='a') as para_handler:
+                para_handler.write(organism+'.'+protein + "\t" + seqio_seq.seq)
+
+
 
 
     print("Total line count:", counter)
+    print("Paracetamol count:", paracetamol_prots_count)
 
 
 
