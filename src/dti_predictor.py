8eb6b62acd64cd1a7aeea9fe380b5a9d6e8a228f
THinnerichs <tilman@hinnerichs.com>
2019-11-06 10:23:04 +0100

    Running line count

--- src/dti_predictor.py
+++ src/dti_predictor.py
@@ -1,1 +1,30 @@
 import numpy as np
+
+
+def main():
+    protein_chemical_links_filename = "../data/protein_chemical.links.transfer.v5.0.tsv"
+
+    paracetamol_id = "CID1983"
+    counter = 0
+    with open(file=protein_chemical_links_filename, read='r') as f:
+        for line in f:
+            counter += 1
+            if counter%10000==0:
+                print("Processed lines:", counter)
+
+            '''
+            if paracetamol_id not in line:
+                continue
+            split_line = line.strip().split('\t')
+            drug = split_line[0]
+            organism, protein = split_line[1].strip().split('.')
+            '''
+
+
+    print("Total line count:", counter)
+
+
+
+
+if __name__=='__main__':
+    main()
