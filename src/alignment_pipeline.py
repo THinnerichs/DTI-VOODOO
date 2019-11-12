a9684c271080b1bc6679c68aba3ad15356ac630d
THinnerichs <tilman@hinnerichs.com>
2019-11-12 12:10:30 +0100

    Now also including aa_seq 2

--- src/alignment_pipeline.py
+++ src/alignment_pipeline.py
@@ -10,14 +10,6 @@ import pickle
 def separate_prots_to_files(file_min_score=400,
                             min_score=400):
 
-    # Create protein amino acid sequence from fasta
-    print("Reading pruned dict...")
-    dict_filename = "../data/prot_aa_seq_dict"
-    protein_aa_seq_dict = None
-    with open(dict_filename + '.pkl', 'rb') as f:
-        protein_aa_seq_dict = pickle.load(f)
-    print("Finished.")
-
     # process drug-protein-interaction file
     print("Processing drug protein data...")
     protein_chemical_links_filename = "../data/protein_chemical.links.min_score_"+str(file_min_score)+".tsv"
@@ -43,18 +35,15 @@ def separate_prots_to_files(file_min_score=400,
             protein = ".".join(split_protein)
             score = int(split_line[10])
 
-            aa_seq = protein_aa_seq_dict[organism][protein]
-
             with open(file="../data/drug_target_relations/"+drug+"_targets", mode='a') as drug_handler:
-                drug_handler.write(organism+'.'+protein + '\t' + aa_seq + '\t'+ str(score) + '\n')
+                drug_handler.write(organism+'.'+protein + '\t' + str(score) + '\n')
     print("Finished.")
 
-def create_fasta(drug_name,
-                 min_score=700):
-    
+def create_fasta_file(drug_name,
+                      min_score=800):
+
     para_filename = "../data/"+drug_name+"_targets"
-    para_fasta_filename = "../data/"+mol_name+"_fasta_" + str(min_score) + "_min_score.fasta"
-    print("Processing {} and writing {} ...".format(para_filename, para_fasta_filename))
+    para_fasta_filename = "../data/"+drug_name+"_fasta_" + str(min_score) + "_min_score.fasta"
     with open(file=para_filename, mode='r') as para_file, open(file=para_fasta_filename, mode='w') as fasta_file:
         for line in para_file:
             protein, aa_seq, score = line.split('\t')
@@ -64,7 +53,8 @@ def create_fasta(drug_name,
             fasta_file.write(">"+protein+'\n')
             fasta_file.write(aa_seq+'\n')
 
-    print("Finished.")
+def msa_from_fasta(min_score=700):
+    pass
 
 if __name__ == '__main__':
     separate_prots_to_files(file_min_score=400,
