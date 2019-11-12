4b36954d68fda99f0d34b5d6f959460ffc9ba8ea
THinnerichs <tilman@hinnerichs.com>
2019-11-12 08:20:29 +0100

    Evaluating all searches 3

--- src/dti_predictor.py
+++ src/dti_predictor.py
@@ -402,6 +402,7 @@ def evaluate_HMMER_search(min_score=700,
                 print("NO MATCHES FOUND!")
                 break
             print(line)
+            print(line.split('\t'))
             protein_id = line.split('\t')[8].strip()
             protein_id_list.append(protein_id)
             if "inclusion threshold" in line:
