6f67cb81132318a9dfd56aa3558882f021acb4a5
THinnerichs <tilman@hinnerichs.com>
2019-11-14 15:21:07 +0100

    Added makeblastdb handling

--- src/similarity_measurement.py
+++ src/similarity_measurement.py
@@ -8,16 +8,23 @@ from joblib import Parallel, delayed
 def test_blast():
     # make_blast_db
     from Bio.Blast.Applications import NcbiblastpCommandline
-    database_fasta_file = "../data/fasta_files/CIDm00000043_fasta_800_min_score.fasta"
-    query_fasta_file = "../data/fasta_files/CIDm00000043_fasta_800_min_score.fasta"
+
+    fasta_path = "../data/fasta_files/"
+    filename = "CIDm00000043_fasta_800_min_score.fasta"
+    database_fasta_file = fasta_path + filename
+    query_fasta_file = fasta_path + filename
+
+    drug_name = filename.split("_")[0]
+    database_name = fasta_path + drug_name + "_blast_db"
 
     # Build blast db
     command = "./makeblastdb -dbtype 'prot' "+\
-              "-in " + database_fasta_file
+              "-in " + database_fasta_file + " "+\
+              "-out " + database_name
     subprocess.call(command, shell=True)
     print(command)
 
-    raise Exception
+    
 
     # cline = NcbiblastpCommandline(query=)
 
