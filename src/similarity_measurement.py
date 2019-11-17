b7bc3d424ed37d7731803a73ebf575cc8bff7fe8
THinnerichs <tilman@hinnerichs.com>
2019-11-17 12:50:44 +0100

    Testing blastp search 1

--- src/similarity_measurement.py
+++ src/similarity_measurement.py
@@ -6,9 +6,11 @@ from joblib import Parallel, delayed
 
 
 def test_blast():
-    # make_blast_db
-    from Bio.Blast.Applications import NcbiblastpCommandline
 
+    # parameters
+    threads = 8
+
+    # make_blast_db
     fasta_path = "../data/fasta_files/"
     filename = "CIDm00000043_fasta_800_min_score.fasta"
     database_fasta_file = fasta_path + filename
@@ -32,6 +34,7 @@ def test_blast():
 
     blast_command = "./blastp "+\
                     "-task blastp-fast "+\
+                    "-num_threads 8 "+\
                     "-query "+query_fasta_file+" "+\
                     "-db "+database_name+" "+\
                     "-out "+results_filename+" "+\
