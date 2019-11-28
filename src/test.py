f8a71fea3d35ccbaf8f1b02faa4d8d6386baa6c4
THinnerichs <tilman@hinnerichs.com>
2019-11-28 11:29:17 +0100

    Testing subset property

---
+++ src/test.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+import os
+
+
+def test_target_subset():
+    path = "../data/drug_target_relations/"
+
+    counter = 0
+
+    for filename in os.listdir(path):
+        if 'm' not in filename:
+            continue
+        s_file = filename.replace('m','s')
+        if os.path.exists(s_file):
+            m_file_targets = set()
+            with open(file=path+filename, mode='r') as m:
+                for line in m:
+                    m_file_targets.add(line.split('\t')[0])
+            s_file_targets = set()
+            with open(file=path+s_file, mode='r') as s:
+                for line in s:
+                    s_file_targets.add(line.split('\t')[0])
+
+            if not s_file_targets.issubset(m_file_targets):
+                counter += 1
+                print(counter, filename)
+
+
+
+if __name__ == '__main__':
+    test_target_subset()
+
+
