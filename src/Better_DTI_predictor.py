47a56a43d2831079937c19269eb7bf75be2011bf
THinnerichs <tilman@hinnerichs.com>
2019-12-12 08:56:40 +0100

    Starting real GCN classifier 4

--- src/Better_DTI_predictor.py
+++ src/Better_DTI_predictor.py
@@ -155,9 +155,6 @@ def better_missing_target_predictor(results_filename = '../results/results_log',
 
         model.summary()
 
-
-        raise Exception
-
         imb_ratio = (len(y_graph_dti_train_data) - y_graph_dti_train_data.sum()) / y_graph_dti_train_data.sum()
 
         class_weight = {0: 1.,
