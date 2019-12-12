0d71834697da0b8d01e6275d376c89bee8496979
THinnerichs <tilman@hinnerichs.com>
2019-12-12 08:11:48 +0100

    Fixed DTI graph 3

--- src/DTI_data_preparation.py
+++ src/DTI_data_preparation.py
@@ -32,8 +32,8 @@ def write_human_DTI_graph(min_score=400):
 
             dti_graph.add_node(drug)
             dti_graph.add_node(target)
-            if score >= min_score:
-                dti_graph.add_edge(drug, target, score=score)
+            # if score >= min_score:
+            dti_graph.add_edge(drug, target, score=score)
 
     print("Finished.\n")
 
