e9650f772d14985a335e9e95ae76292c841bdef5
THinnerichs <tilman@hinnerichs.com>
2019-12-16 09:58:03 +0100

    Restarting GAT model 5

--- src/Better_DTI_predictor.py
+++ src/Better_DTI_predictor.py
@@ -115,28 +115,36 @@ def better_missing_target_predictor(results_filename = '../results/results_log',
         # Build actual dti model
         GCN_layer_sizes = embedding_layer_sizes[:]
         PPI_input = layers.Input(shape=(node_feature_mat.shape[1],))
-        graph_layer = layers.Dropout(0.01)(PPI_input)
+        graph_layer = layers.Dropout(0.)(PPI_input)
 
-        if embedding_method == 'gcn':
-            graph_layer = GraphCNN(GCN_layer_sizes.pop(0), num_filters, graph_conv_filters, kernel_regularizer=regularizers.l2(5e-1),
-                                   activation='elu')(PPI_input)
+        '''
+        # if embedding_method == 'gcn':
+        graph_layer = GraphCNN(GCN_layer_sizes.pop(0), num_filters, graph_conv_filters, kernel_regularizer=regularizers.l2(5e-1),
+                               activation='elu')(PPI_input)
+        graph_layer = layers.Dropout(0.2)(graph_layer)
+
+        for size in GCN_layer_sizes:
+            graph_layer = GraphCNN(size, num_filters, graph_conv_filters, kernel_regularizer=regularizers.l2(5e-2),
+                                   activation='elu')(graph_layer)
+
             graph_layer = layers.Dropout(0.2)(graph_layer)
+        '''
+        # elif embedding_method == 'gat':
+        graph_layer = GraphAttentionCNN(GCN_layer_sizes.pop(0),
+                                        num_filters,
+                                        graph_conv_filters,
+                                        num_attention_heads=8,
+                                        attention_combine='concat',
+                                        attention_dropout=0.6,
+                                        kernel_regularizer=regularizers.l2(5e-1),
+                                        activation='elu')(graph_layer)
+        graph_layer = layers.Dropout(0.4)(graph_layer)
 
-            for size in GCN_layer_sizes:
-                graph_layer = GraphCNN(size, num_filters, graph_conv_filters, kernel_regularizer=regularizers.l2(5e-2),
-                                       activation='elu')(graph_layer)
+        for size in GCN_layer_sizes:
+            graph_layer = GraphCNN(size, num_filters, graph_conv_filters, kernel_regularizer=regularizers.l2(5e-2),
+                                   activation='elu')(graph_layer)
 
-                graph_layer = layers.Dropout(0.2)(graph_layer)
-        elif embedding_method == 'gat':
-            graph_layer = GraphAttentionCNN(GCN_layer_sizes.pop(0),
-                                            num_filters,
-                                            graph_conv_filters,
-                                            num_attention_heads=8,
-                                            attention_combine='concat',
-                                            attention_dropout=0.6,
-                                            kernel_regularizer=regularizers.l2(5e-1),
-                                            activation='elu')(graph_layer)
-            graph_layer = layers.Dropout(0.4)(graph_layer)
+            graph_layer = layers.Dropout(0.2)(graph_layer)
 
         DDI_input = layers.Input(shape=(len(drug_list),))
 
