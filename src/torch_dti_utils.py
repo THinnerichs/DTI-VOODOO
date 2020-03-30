c6020399eae237fde5ebc83fa7b350f4aa2085e1
THinnerichs <tilman@hinnerichs.com>
2020-03-30 10:47:38 +0200

    Running transductive inference 13

--- src/torch_dti_utils.py
+++ src/torch_dti_utils.py
@@ -151,8 +151,8 @@ def train(model, device, train_loader, optimizer, epoch):
         data = data.to(device)
         optimizer.zero_grad()
         output = model(data)
-        # print('output.size()', output.size(), data.y.view(-1, 1).size())
-        loss = F.nll_loss(output.view(-1), data.y.view(-1).float().to(device))
+        print('output.size()', output.size(), data.y.view(-1, 1).size())
+        loss = F.nll_loss(output.view(-1, 1), data.y.view(-1, 1).float().to(device))
         loss.backward()
         optimizer.step()
         if batch_idx % 1 == 0:
