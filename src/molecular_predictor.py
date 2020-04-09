6f33033a9baa6ab4d9f7057325fd6ec356371cdd
THinnerichs <tilman@hinnerichs.com>
2020-04-09 17:59:30 +0200

    Building drug representation 7

--- src/molecular_predictor.py
+++ src/molecular_predictor.py
@@ -54,8 +54,11 @@ def encode_drugs(drug_list,
         trfm.eval()
 
         x_split = [split(sm) for sm in [drug_SMILES_dict[drug] for drug in drug_list]]
+        print(len(x_split), type(x_split))
         xid, xseg = get_array(x_split)
+        print(xid, type(xid))
         X = trfm.encode(torch.t(xid))
+        print(X)
 
         print('Size:', X.size())
 
