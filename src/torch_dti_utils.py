ff70cbd99816767064f663100012a912ffbe96f7
THinnerichs <tilman@hinnerichs.com>
2020-04-15 18:25:05 +0200

    Test GCN over Mol predictions 4

--- src/torch_dti_utils.py
+++ src/torch_dti_utils.py
@@ -206,6 +206,7 @@ class MolPredDTINetworkData():
             DDI_features = torch.tensor(self.DDI_features[:, drug_index], dtype=torch.float).view(1, self.num_drugs)
 
             feature_array = torch.tensor(self.mol_predictions[drug_index, :self.num_proteins], dtype=torch.float).round()
+            print('feature array', feature_array.size())
             full_PPI_graph = Data(x=feature_array, edge_index=self.edge_list, y=y)
             full_PPI_graph.DDI_features = DDI_features
             full_PPI_graph.protein_mask = protein_mask
