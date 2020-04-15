05016fb9a4237ed95814b808e84861fdd2d8ce91
THinnerichs <tilman@hinnerichs.com>
2020-04-15 19:10:41 +0200

    Test GCN over Mol predictions 6

--- src/torch_networks.py
+++ src/torch_networks.py
@@ -64,7 +64,7 @@ class TemplateSimpleNet(torch.nn.Module):
             raise ValueError
 
 
-        self.fc_g1 = torch.nn.Linear(num_features*2, 512)
+        self.fc_g1 = torch.nn.Linear(num_features*256, 512)
         self.fc_g2 = torch.nn.Linear(512, GCN_num_outchannels)
 
         self.relu = torch.nn.ReLU()
@@ -81,9 +81,6 @@ class TemplateSimpleNet(torch.nn.Module):
         # print('protein_mask.size()', protein_mask.size())
 
         # PPI graph network
-        print('PPI_x.size', PPI_x.size(), PPI_edge_index.size())
-        PPI_x = self.conv1(PPI_x, PPI_edge_index)
-        PPI_x = F.relu(PPI_x)
         # PPI_x = F.dropout(PPI_x, training=self.training)
         PPI_x = self.conv2(PPI_x, PPI_edge_index)
         PPI_x = F.relu(PPI_x)
