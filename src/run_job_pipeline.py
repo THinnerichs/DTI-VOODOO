cd210e798cbf64ac3892d58e8048872ac66c2eb1
THinnerichs <tilman@hinnerichs.com>
2020-04-28 10:10:02 +0200

    Resubmit GCNConv job without data leak 1

--- src/run_job_pipeline.py
+++ src/run_job_pipeline.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
     # cancel_jobs()
     # submit_jobscript_n_times(50)
 
-    # cancel_jobs()
+    cancel_jobs()
 
     # for fold in range(1,6):
         # submit_protfunc_pred_job(epochs=30, batch_size=131072, fold=fold, num_gpus=4, days=1)
@@ -245,7 +245,7 @@ if __name__ == '__main__':
             # submit_gpu_job(epochs=30, batch_size=32, days=2, arch=arch, mode='drug', fold=fold, num_gpus=2, neg_sample_ratio=0.05)
             # submit_gpu_job(epochs=30, batch_size=32, days=2, arch='Res'+arch, mode='drug', fold=fold, num_gpus=2, neg_sample_ratio=0.05)
 
-            submit_gpu_job(epochs=10, batch_size=180, mem=370, days=1, arch=arch, fold=fold, num_gpus=4, neg_sample_ratio=0.05, model_id='ResBlocks')
+            submit_gpu_job(epochs=10, batch_size=160, mem=360, days=1, arch=arch, fold=fold, num_gpus=4, neg_sample_ratio=0.05, model_id='ResBlocks')
             # submit_gpu_job(epochs=30, batch_size=160, mem=360, days=2, arch=arch, fold=fold, num_gpus=4, neg_sample_ratio=0.1)
             # submit_gpu_job(num_proteins=4000, epochs=30, batch_size=64, arch=arch)
             # submit_gpu_job(num_proteins=1000, epochs=30, batch_size=256, arch=arch)
