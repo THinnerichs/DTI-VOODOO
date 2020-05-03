6bff9dd487bac4c13552f5bc9c0ba7db26fa22f6
THinnerichs <tilman@hinnerichs.com>
2020-05-03 12:56:41 +0200

    Running SAGEConv with deeper post evaluation 4

--- src/torch_networks.py
+++ src/torch_networks.py
@@ -35,11 +35,11 @@ class TemplateSimpleNet(torch.nn.Module):
             self.conv3 = nn.ChebConv(num_features*16, num_features*128, 3)
         elif 'SAGEConv' in conv_method:
             self.conv1 = nn.SAGEConv(num_features, num_features*4)
-            self.conv2 = nn.SAGEConv(num_features*4, num_features*64)
+            self.conv2 = nn.SAGEConv(num_features*4, num_features*16)
             self.conv3 = nn.SAGEConv(num_features*16, num_features*128)
         elif 'GraphConv' in conv_method:
             self.conv1 = nn.GraphConv(num_features, num_features*4)
-            self.conv2 = nn.GraphConv(num_features*4, num_features*64)
+            self.conv2 = nn.GraphConv(num_features*4, num_features*16)
             self.conv3 = nn.GraphConv(num_features*16, num_features*128)
         elif 'GATConv' in conv_method:
             self.conv1 = nn.GATConv(num_features, num_features*4, heads=5)
@@ -47,15 +47,15 @@ class TemplateSimpleNet(torch.nn.Module):
             self.conv3 = nn.GATConv(num_features*16, num_features*128, heads=5)
         elif 'TAGConv' in conv_method:
             self.conv1 = nn.TAGConv(num_features, num_features*4)
-            self.conv2 = nn.TAGConv(num_features*4, num_features*64)
+            self.conv2 = nn.TAGConv(num_features*4, num_features*16)
             self.conv3 = nn.TAGConv(num_features*16, num_features*128)
         elif 'ARMAConv' in conv_method:
             self.conv1 = nn.ARMAConv(num_features, num_features*4)
-            self.conv2 = nn.ARMAConv(num_features*4, num_features*64)
+            self.conv2 = nn.ARMAConv(num_features*4, num_features*16)
             self.conv3 = nn.ARMAConv(num_features*16, num_features*128)
         elif 'SGConv' in conv_method:
             self.conv1 = nn.SGConv(num_features, num_features*4)
-            self.conv2 = nn.SGConv(num_features*4, num_features*64)
+            self.conv2 = nn.SGConv(num_features*4, num_features*16)
             self.conv3 = nn.SGConv(num_features*16, num_features*128)
         elif 'FeaStConv' in conv_method:
             self.conv1 = nn.FeaStConv(num_features, num_features*4, heads=5)
