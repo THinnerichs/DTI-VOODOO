28afdada27f6e795d60a5b966f85243996ffdaf8
THinnerichs <tilman@hinnerichs.com>
2020-05-06 11:11:49 +0200

    Check output for infeasible values 2

--- src/torch_dti_predictor.py
+++ src/torch_dti_predictor.py
@@ -179,11 +179,11 @@ def transductive_missing_target_predictor(config,
                     else:
                         print(test_loss, 'No improvement since epoch ', best_epoch, ';', model_st)
 
-                    model_filename = '../models/PPI_network_' + config.arch + '_'+str(epoch)+'_epochs_model_fold_' + str(fold) + '.model'
+                    model_filename = '../models/PPI_network_' + (config.model_id+'_' if config.model_id else '') + config.arch + '_'+str(epoch)+'_epochs_model_fold_' + str(fold) + '.model'
                     torch.save(model.state_dict(), model_filename)
 
             if epoch%5 == 0:
-                model_filename = '../models/PPI_network_' + config.arch + '_' + str(epoch) + '_epochs_model_fold_' + str(fold) + '.model'
+                model_filename = '../models/PPI_network_' + (config.model_id+'_' if config.model_id else '') + config.arch + '_' + str(epoch) + '_epochs_model_fold_' + str(fold) + '.model'
                 torch.save(model.state_dict(), model_filename)
 
             sys.stdout.flush()
