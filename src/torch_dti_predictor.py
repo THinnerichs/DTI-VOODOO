9fa17a8a060b9e9e10f61b39791de6642674164c
THinnerichs <tilman@hinnerichs.com>
2020-05-25 09:48:49 +0200

    Sanity check for protein labels 11

--- src/torch_dti_predictor.py
+++ src/torch_dti_predictor.py
@@ -153,7 +153,7 @@ def transductive_missing_target_predictor(config,
             print('Train loss:', loss)
             sys.stdout.flush()
 
-            if epoch%config.num_epochs == 0:
+            if epoch%10 == 0:
                 print('Predicting for validation data...')
                 file='../results/interactions_results_' +config.arch+'_'+ str(num_proteins) + '_prots_'+str(epoch)+'_epochs'
                 with open(file=file, mode='a') as f:
@@ -162,14 +162,14 @@ def transductive_missing_target_predictor(config,
                           metrics.accuracy_score(train_labels, train_predictions),
                           dti_utils.dti_auroc(train_labels, train_predictions),
                           dti_utils.dti_f1_score(train_labels, train_predictions),
-                          metrics.matthews_corrcoef(train_labels, train_predictions), file=f)
+                          metrics.matthews_corrcoef(train_labels, train_predictions))#@TODO, file=f)
 
                     test_labels, test_predictions = predicting(model, device, test_loader)
                     print(config.model_id, 'Test:', config.neg_sample_ratio,'Acc, ROC_AUC, f1, matthews_corrcoef',
                           metrics.accuracy_score(test_labels, test_predictions),
                           dti_utils.dti_auroc(test_labels, test_predictions),
                           dti_utils.dti_f1_score(test_labels, test_predictions),
-                          metrics.matthews_corrcoef(test_labels, test_predictions), file=f)
+                          metrics.matthews_corrcoef(test_labels, test_predictions))#@TODO, file=f)
 
                     metrics_func_list = [metrics.accuracy_score, dti_utils.dti_auroc, dti_utils.dti_f1_score,
                                          metrics.matthews_corrcoef]
