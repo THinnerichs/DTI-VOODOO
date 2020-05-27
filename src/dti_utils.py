9e00d34fe90e4a14ca2e376633851e6ada7b1a57
THinnerichs <tilman@hinnerichs.com>
2020-05-27 09:20:21 +0200

    Run quick node classification model 4

--- src/dti_utils.py
+++ src/dti_utils.py
@@ -10,6 +10,8 @@ import math
 def dti_auroc(y_true, y_pred):
     if len(np.unique(y_true)) == 1:  # bug in roc_auc_score
         return 0.5
+    if y_pred.sum() == 0 or (1-y_pred).sum() == 0 or y_true.sum() == 0 or (1-y_true).sum()==0:
+        return 0.5
     return metrics.roc_auc_score(y_true, y_pred)
 
 def dti_f1_score(y_true, y_pred):
