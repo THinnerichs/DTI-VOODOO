216ca70945874eb42d001cbae279db6e7d3152b9
THinnerichs <tilman@hinnerichs.com>
2020-06-24 18:07:32 +0200

    Testing MedDRA graph 2

--- src/similarity_measurement.py
+++ src/similarity_measurement.py
@@ -215,10 +215,12 @@ def write_enriched_SIDER_graph():
     UMLS_to_MedDRA_id_dict = {}
     for UMLSid, MedDRAid in qres:
         UMLS_to_MedDRA_id_dict[UMLSid.value] = MedDRAid
+        print(UMLSid)
+        print(UMLSid.value)
         print(MedDRAid)
-        print(MedDRAid.value)
 
-    raise Exception
+        raise Exception
+
 
     updated_SIDER_graph = get_updated_MedDRA_label_SIDER_graph()
     drug_list = get_SIDER_drug_list()
