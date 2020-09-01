24108bf5fbb6c1a9b40067267cb75742e65ef0c1
THinnerichs <tilman@hinnerichs.com>
2020-09-01 19:32:42 +0200

    Added node degree to datasets 8

--- src/torch_dti_utils.py
+++ src/torch_dti_utils.py
@@ -572,6 +572,7 @@ class QuickProtFuncDTINetworkData:
             degree_feature = self.node_degree_protein_feature
 
             # uncomment for DL2vec
+            print(degree_feature.size(), drug_feature.size(), protein_feature.size())
             feature_array = torch.cat([degree_feature, drug_feature, protein_feature], dim=1)
             # feature_array = torch.tensor(feature_array, dtype=torch.float)
 
