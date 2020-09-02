3638cd1feb15331d7c060a0eed1bd094d835c0ab
THinnerichs <tilman@hinnerichs.com>
2020-09-02 12:22:31 +0200

    Rerun HPO classifier with degree stuff 7

--- src/protein_function_predictor.py
+++ src/protein_function_predictor.py
@@ -237,7 +237,7 @@ def drug_split_protein_function_predictor(config):
                           metrics.accuracy_score(train_labels, train_predictions),
                           dti_utils.dti_auroc(train_labels, train_predictions),
                           dti_utils.dti_f1_score(train_labels, train_predictions),
-                          metrics.matthews_corrcoef(train_labels, train_predictions), file=f)
+                          metrics.matthews_corrcoef(train_labels, train_predictions))#@TODO, file=f)
 
                     test_labels, test_predictions = predicting(model, device, test_loader)
                     print('Test:', config.include_uberon, config.include_GO, config.include_phenotype,
@@ -245,7 +245,7 @@ def drug_split_protein_function_predictor(config):
                           metrics.accuracy_score(test_labels, test_predictions),
                           dti_utils.dti_auroc(test_labels, test_predictions),
                           dti_utils.dti_f1_score(test_labels, test_predictions),
-                          metrics.matthews_corrcoef(test_labels, test_predictions), file=f)
+                          metrics.matthews_corrcoef(test_labels, test_predictions))#@TODO, file=f)
 
                     metrics_func_list = [metrics.accuracy_score, dti_utils.dti_auroc, dti_utils.dti_f1_score,
                                          metrics.matthews_corrcoef]
