88f76beaab286d90ea030bdd5f4b6f002ba5cfdc
THinnerichs <tilman@hinnerichs.com>
2020-10-30 16:33:17 +0100

    Testing different layer types (GATConv) 2

--- src/torch_networks.py
+++ src/torch_networks.py
@@ -446,9 +446,9 @@ class QuickTemplateNodeFeatureNet(torch.nn.Module):
             self.conv2 = nn.GCNConv(8, 8, cached=False,  add_self_loops=True)
             self.conv3 = nn.GCNConv(8, 1, cached=False, add_self_loops=True)
         elif 'GATConv' in conv_method:
-            self.conv1 = nn.GATConv(1, 4, heads=8, dropout=0.2)
-            self.conv2 = nn.GATConv(8*4, 4, heads=8, dropout=0.2)
-            self.conv3 = nn.GATConv(8*4, 1, heads=1)
+            self.conv1 = nn.GATConv(1, 2, heads=8, dropout=0.2)
+            self.conv2 = nn.GATConv(8*2, 2, heads=8, dropout=0.2)
+            self.conv3 = nn.GATConv(8*2, 1, heads=1)
         else:
             print("No valid model selected.")
             sys.stdout.flush()
