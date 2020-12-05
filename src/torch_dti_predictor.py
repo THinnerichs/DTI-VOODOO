2164416d60be406017fb598c7a720703b8235310
THinnerichs <tilman@hinnerichs.com>
2020-12-05 15:50:23 +0100

    Test GENConv with freezing and 900 PPI_min_score 2

--- src/torch_dti_predictor.py
+++ src/torch_dti_predictor.py
@@ -642,7 +642,6 @@ if __name__ == '__main__':
     parser.add_argument("--fold", type=int, default=-1)
 
     parser.add_argument("--split_mode", type=str, default='standard')
-    parser.add_argument("--drug_mode", type=str, default='standard')
     parser.add_argument("--pretrain", type=bool, default=True)
     parser.add_argument("--model_id", type=str, default='')
 
@@ -650,8 +649,8 @@ if __name__ == '__main__':
     parser.add_argument("--mode", type=str, default='')
     parser.add_argument("--PPI_min_score", type=int, default=700)
 
-    parser.add_argument("--include_mol_features", action='store_true')
     parser.add_argument("--drug_mode", type=str, default='trfm')
+    parser.add_argument("--include_mol_features", action='store_true')
 
 
     config = parser.parse_args()
