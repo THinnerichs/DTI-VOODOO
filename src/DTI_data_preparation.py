09cf83a4838cf732dfb94282bec7fb0d40e42f52
THinnerichs <tilman@hinnerichs.com>
2020-12-08 17:48:09 +0100

    Analyze graph model predictions 1

--- src/DTI_data_preparation.py
+++ src/DTI_data_preparation.py
@@ -437,10 +437,36 @@ def get_protein_degree_percentile(protein_list, n=10, PPI_min_score=700):
 
     return return_matrix
 
+def get_drug_to_name_mapping():
+    drug_to_IUPAC_id_mapping = {}
 
+    filename='../data/STITCH_data/chemical.aliases.v5.0.tsv'
+    print('Loading drug to IUPAC dict...')
+    with open(file=filename, mode='r') as f:
+        # skip header
+        f.readline()
 
+        for line in tqdm(f, total=174324327):
+            drug_id, _, alias, source = line.strip().split('\t')
+            if 'IUPAC' in source:
+                drug_to_IUPAC_id_mapping[drug_id.strip()] = alias.strip()
 
+    return drug_to_IUPAC_id_mapping
 
+def get_protein_to_EnsemblProtein_id():
+    protein_to_Ensembl_protein_id_mapping = {}
+
+    filename = '../data/STRING_data/9606.protein.aliases.v11.0.txt'
+    with open(file=filename, mode='r') as f:
+        # skip header
+        f.readline()
+
+        for line in tqdm(f, total=2224814):
+            protein_id, alias, source = line.strip().split('\t')
+            if source.strip() == 'Ensembl_protein_id':
+                protein_to_Ensembl_protein_id_mapping[protein_id.strip()] = alias.strip()
+
+    return protein_to_Ensembl_protein_id_mapping
 
 
 def test():
--- src/results_evaluation.py
+++ src/results_evaluation.py
@@ -1,13 +1,49 @@
 import pickle
 import numpy as np
+import random
+
+import DTI_data_preparation
 
 def write_predicted_DTIs(fold=3):
-    filename = 'PPI_network_model_with_mol_features_fold_'+str(fold)+'_predictions.pkl'
+    filename = '../models/graph_models/PPI_network_model_with_mol_features_fold_'+str(fold)+'_predictions.pkl'
+
+    drug_mapping = DTI_data_preparation.get_drug_to_name_mapping()
+    protein_mapping = DTI_data_preparation.get_protein_to_EnsemblProtein_id()
+
+    print('drug_dict.keys()', len(list(drug_mapping.keys())))
+    print('protein_dict.keys()', len(list(protein_mapping.keys())))
 
     pred_list = None #[(drug, prot, label, pred)]
     with open(file=filename, mode='rb') as f:
         pred_list = pickle.load(f)
 
-    pred_list = [tup for tup in pred_list if tup[label]]
+    drug_list = [tup[0] for tup in pred_list]
+    protein_list = [tup[1] for tup in pred_list]
 
-    pred_list = sorted(pred_list, key=lambda tup: tup)
+    print('drug_list', len(drug_list))
+    print('protein_list', len(protein_list))
+    print('intersection drug', len(set(drug_list) & set(drug_mapping.keys())))
+    print('intersection protein', len(set(protein_list) & set(protein_mapping.keys())))
+
+
+    pos_sanity_list = [tup for tup in pred_list if round(tup[2] == 1)]
+    random.shuffle(pos_sanity_list)
+    print('sanity_check', pos_sanity_list[:200])
+
+
+    pred_list = [tup for tup in pred_list if round(tup[2])==0 and tup[3] > 0.7]
+
+    pred_list = sorted(pred_list, key=lambda tup: tup[3], reverse=True)
+    print('len(pred_list):', len(pred_list))
+
+    filename = '../results/full_model_with_mol_feat_results/best_preds_false_negatives'
+    with open(file=filename, mode='w') as f:
+        print('drug\tprot\tconfidence\tdrug_alias\tprotein_alias', file=f)
+        for drug, protein, _, confidence in pred_list:
+            if drug in protein_mapping.keys() and protein in protein_mapping.keys():
+                print('\t'.join([drug, protein, str(confidence), drug_mapping[drug], protein_mapping[protein]]), file=f)
+
+
+
+
+
