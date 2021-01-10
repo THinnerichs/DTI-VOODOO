b2fd642183c8ff7fea5e0adf9bc651438f120bed
THinnerichs <tilman@hinnerichs.com>
2021-01-10 15:45:28 +0100

    Submit HPO pretraining for full yamanishi 7

--- src/DTI_data_preparation.py
+++ src/DTI_data_preparation.py
@@ -400,7 +400,8 @@ def get_yamanishi_data(original_drug_list, original_protein_list):
     protein_list = list(map(lambda p: yamanishi_protein_mapping.get(p, None), protein_list))
 
     drug_indices = [drug_list.index(drug) for drug in drug_list if drug!=None and not drug.startswith('1')]
-    protein_indices = [protein_list.index(protein) for protein in protein_list if protein in original_protein_list]
+    intersec = list(set(original_protein_list) & set(protein_list))
+    protein_indices = [protein_list.index(protein) for protein in protein_list if protein in intersec]
 
     drug_list = np.array(drug_list)
     protein_list = np.array(protein_list)
