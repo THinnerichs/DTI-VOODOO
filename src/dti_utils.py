394509c262f772114bd8db0d0f0acee814436b9c
THinnerichs <tilman@hinnerichs.com>
2021-01-12 14:11:39 +0100

    Retest on NeoDTI data 11

--- src/dti_utils.py
+++ src/dti_utils.py
@@ -8,8 +8,10 @@ import math
 
 
 def dti_auroc(y_true, y_pred):
-    if len(np.unique(y_true)) == 1 or len(np.unique(y_pred)==1):  # bug in roc_auc_score
+    if len(np.unique(y_true)) == 1:  # bug in roc_auc_score
         return 0.5
+    if len(np.unique(y_pred)) == 0:
+        print('y_pred.sum()', np.unique(y_pred), y_pred.sum())
     if y_pred.sum() == 0 or (1-y_pred).sum() == 0 or y_true.sum() == 0 or (1-y_true).sum()==0:
         return 0.5
     return metrics.roc_auc_score(y_true, y_pred)
