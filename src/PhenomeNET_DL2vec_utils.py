80bd58dd3284b3caa21ae5b479aea2bb32b67ab9
THinnerichs <tilman@hinnerichs.com>
2021-01-15 14:51:34 +0100

    Start DL2vec on UMLS 6

--- src/PhenomeNET_DL2vec_utils.py
+++ src/PhenomeNET_DL2vec_utils.py
@@ -91,12 +91,12 @@ def write_UMLS_NET_files():
     disease_list, dii_matrix = parse_drug_indications()
     drug_list = DDI_utils.get_yamanishi_drug_list()
 
-    drug_indices = [drug_list.index(drug) for drug in drug_list if drug!=None and not drug.startswith('1')]
+    drug_indices = [drug_list.index(drug) for drug in list(drug_list) if drug!=None and not drug.startswith('1')]
     drug_list = drug_list[drug_indices]
     dii_matrix = dii_matrix[drug_indices, :]
 
     print('drug_list:', drug_list.shape)
-    print('dii_list:', dii_matrix.shape)
+    print('pruned dii_list:', dii_matrix.shape)
 
     drug_indication_pairs = []
     for drug_index in range(len(drug_list)):
