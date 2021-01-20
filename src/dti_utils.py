09d8a60d9eef0aab9b2a635c82a9c12a12080cde
THinnerichs <tilman@hinnerichs.com>
2021-01-20 16:49:20 +0100

    Run overall predictor on freshly updated SIDER embeddings 1

--- src/dti_utils.py
+++ src/dti_utils.py
@@ -11,18 +11,25 @@ def dti_auroc(y_true, y_pred):
     return metrics.roc_auc_score(y_true, y_pred)
 
 def dti_auprc(y_true, y_pred):
-    if y_pred.sum() == 0 or (1-y_pred).sum() == 0 or y_true.sum() == 0 or (1-y_true).sum()==0:
-        return 0.5
-    # p,r, t = metrics.precision_recall_curve(y_true, y_pred)
-    # return metrics.auc(r, p)
-    return metrics.average_precision_score(y_true, y_pred, average='weighted')
+    p,r, t = metrics.precision_recall_curve(y_true, y_pred)
+    return metrics.auc(r, p)
+    # return metrics.average_precision_score(y_true, y_pred, average='weighted')
 
 def dti_mcc(y_true, y_pred):
-    if y_pred.sum() == 0 or (1-y_pred).sum() == 0 or y_true.sum() == 0 or (1-y_true).sum()==0:
-        return 0.
     return metrics.matthews_corrcoef(y_true, y_pred)
 
 def dti_f1_score(y_true, y_pred):
-    if y_pred.sum() == 0 or (1-y_pred).sum() == 0 or y_true.sum() == 0 or (1-y_true).sum()==0:
-        return 0.
     return metrics.f1_score(y_true, y_pred)
+
+def micro_AUC_per_prot(y_true, y_pred, num_drugs):
+    y_true = y_true.reshape(num_drugs, -1)
+    y_pred = y_pred.reshape(num_drugs, -1)
+
+    num_prots = y_true.shape[1]
+    prot_wise_auc = []
+    for prot_index in range(num_prots):
+        prot_wise_auc.append(metrics.roc_auc_score(y_true[:, prot_index], y_pred[:, prot_index]))
+
+
+
+
--- src/torch_dti_utils.py
+++ src/torch_dti_utils.py
@@ -27,7 +27,12 @@ class QuickProtFuncDTINetworkData:
         self.config = config
 
         print("Loading data ...")
-        self.drug_list = np.array(DTI_data_preparation.get_drug_list(config.mode))
+        if config.yamanishi_test:
+            self.drug_list = np.array(PhenomeNET_DL2vec_utils.get_PhenomeNET_drug_list())
+        else:
+            self.drug_list = np.array(DTI_data_preparation.get_drug_list(config.mode))
+        print(len(self.drug_list), "drugs present")
+
         print(len(self.drug_list), "drugs present")
         # self.protein_list = np.array(DTI_data_preparation.get_human_prot_func_proteins())[:config.num_proteins]
         # get protein lists for each ontology
@@ -64,11 +69,8 @@ class QuickProtFuncDTINetworkData:
         if config.yamanishi_test:
             self.protein_list = np.array(list(set(self.PPI_graph.nodes()) & (set(uberon_protein_list) | set(GO_protein_list) | set(MP_protein_list))))
             print("Loading Yamanishi data ...")
-            orig_drug_list = self.drug_list
             self.drug_list, self.protein_list, self.y_dti_data = DTI_data_preparation.get_yamanishi_data(self.drug_list, self.protein_list)
 
-            print(set(self.drug_list) - set(orig_drug_list))
-            raise Exception
             print(self.drug_list.shape, self.y_dti_data.shape, self.protein_list.shape)
 
             if config.include_mol_features:
