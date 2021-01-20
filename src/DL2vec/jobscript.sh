7ae15a36270e079835f4a286f92cbe9f74b60217
THinnerichs <tilman@hinnerichs.com>
2021-01-20 21:43:51 +0100

    Update drug smiles mapping according to updated SIDER drugs 4

--- src/DL2vec/jobscript.sh
+++ src/DL2vec/jobscript.sh
@@ -5,8 +5,8 @@
 #SBATCH -o ../jobscript_outputs/DL2vec.%J.out
 #SBATCH -e ../jobscript_outputs/DL2vec.%J.err
 #SBATCH --time=0-24:00:00
-#SBATCH --mem=350G
-#SBATCH --cpus-per-task=40
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=64
 
 #run the application:
 source /home/hinnertr/.bashrc
@@ -14,7 +14,7 @@ module load gcc/6.4.0
 conda activate ~/.conda/envs/dti/
 module load groovy/3.0.6
 
-# python runDL2vec.py -embedsize 200 -ontology ../../data/drug_indications/UMLS.owl -associations ../../data/drug_indications/drug_indication_association_file -outfile ../../data/drug_indications/embedding_model -entity_list ../../data/drug_indications/drug_indication_entity_list -num_workers 40
+python runDL2vec.py -embedsize 200 -ontology ../../data/drug_indications/UMLS.owl -associations ../../data/drug_indications/drug_indication_association_file -outfile ../../data/drug_indications/embedding_model -entity_list ../../data/drug_indications/drug_indication_entity_list -num_workers 64 -file_prefix drug_ind_
 
-python runDL2vec.py -embedsize 200 -num_workers 40 -ontology ../../data/PhenomeNET_data/phenomenet.owl -associations ../../data/PhenomeNET_data/drug_association_file -outfile ../../data/PhenomeNET_data/drug_embedding_model -entity_list ../../data/PhenomeNET_data/drug_entity_list -file_prefix drug_
+# python runDL2vec.py -embedsize 200 -num_workers 40 -ontology ../../data/PhenomeNET_data/phenomenet.owl -associations ../../data/PhenomeNET_data/drug_association_file -outfile ../../data/PhenomeNET_data/drug_embedding_model -entity_list ../../data/PhenomeNET_data/drug_entity_list -file_prefix drug_
 
