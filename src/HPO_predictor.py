270242cacc6a103e2d31f1a5be10ceffd0dbf24a
THinnerichs <tilman@hinnerichs.com>
2021-03-29 11:38:18 +0200

    Fix typo in graph convolution biosnap data preparation 2

--- src/HPO_predictor.py
+++ src/HPO_predictor.py
@@ -232,7 +232,7 @@ class HPOPredNet(nn.Module):
         self.model2 = nn.Sequential(
             nn.Linear(600, 256),
             # nn.Dropout(0.5),
-            nn.BatchNorm1d(256, affine=True),
+            # nn.BatchNorm1d(256, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(256, 200),
             # nn.BatchNorm1d(200),
